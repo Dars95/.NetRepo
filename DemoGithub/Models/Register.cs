412c89b43f3d9047b44af3059f0007aee935898d
Darshal <dhruv.chavda@thedestinysolutions.com>
2024-04-16 16:24:57 +0530

    Add Register model

---
+++ DemoGithub/Models/Register.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoGithub.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime Created { get; set; }
+
+        = DateTime.Now;
+
+            
+
+        public DateTime Updated { get; set; }
+        = DateTime.Now;
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        = string.Empty;
+        public string PasswordHash { get; set; }
+        = string.Empty;
+            
+        public string EmailHash { get; set; }
+            
+        public string PasswordSalt { get; set; }
+        public string PasswordSaltHash { get; set;
+        
+            
+        
+    }
+    }
+}
