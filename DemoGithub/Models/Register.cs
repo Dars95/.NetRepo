afc17118f5f729b2fada1e615f763d7717d8c881
Darshal <dhruv.chavda@thedestinysolutions.com>
2024-04-16 16:31:22 +0530

    Changes in Models

--- DemoGithub/Models/Register.cs
+++ DemoGithub/Models/Register.cs
@@ -12,26 +12,6 @@ namespace DemoGithub.Models
         public string Name { get; set; }
         
         public string Description { get; set; }
-
-        public string Author { get; set; }
-
-        public DateTime Created { get; set; }
-
-        = DateTime.Now;
-
-            
-
-        public DateTime Updated { get; set; }
-        = DateTime.Now;
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Username { get; set; }
-        = string.Empty;
-        public string PasswordHash { get; set; }
-        = string.Empty;
-            
-        public string EmailHash { get; set; }
-            
         public string PasswordSalt { get; set; }
         public string PasswordSaltHash { get; set;
         
